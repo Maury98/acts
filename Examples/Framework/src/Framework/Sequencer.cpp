0e062fb2f7e04d096450e91896a82e6284610532
Paul Gessinger <paul.gessinger@cern.ch>
2023-03-14 17:01:52 +0100

    fix: Sequencer use-after-move in sequencer data flow check (#1941)
    If Sequencer data flow checks are turned on, there's a use-after-move bug that this PR fixes.

--- Examples/Framework/src/Framework/Sequencer.cpp
+++ Examples/Framework/src/Framework/Sequencer.cpp
@@ -102,13 +102,107 @@ void Sequencer::addWriter(std::shared_ptr<IWriter> writer) {
   addElement(std::move(writer));
 }
 
-void Sequencer::addElement(std::shared_ptr<SequenceElement> element) {
+void Sequencer::addElement(const std::shared_ptr<SequenceElement>& element) {
   if (not element) {
     throw std::invalid_argument("Can not add empty/NULL element");
   }
-  m_sequenceElements.push_back(std::move(element));
-  ACTS_INFO("Added " << getAlgorithmType(*m_sequenceElements.back()) << " '"
-                     << m_sequenceElements.back()->name() << "'");
+
+  m_sequenceElements.push_back(element);
+
+  if (!m_cfg.runDataFlowChecks) {
+    return;
+  }
+
+  bool valid = true;
+  std::string elementType{getAlgorithmType(*element)};
+  std::string elementTypeCapitalized = elementType;
+  elementTypeCapitalized[0] = std::toupper(elementTypeCapitalized[0]);
+
+  for (const auto* handle : element->readHandles()) {
+    if (!handle->isInitialized()) {
+      continue;
+    }
+
+    if (auto it = m_whiteBoardState.find(handle->key());
+        it != m_whiteBoardState.end()) {
+      const std::type_info& type = *it->second;
+      if (type != handle->typeInfo()) {
+        ACTS_ERROR("Adding "
+                   << elementType << " " << element->name() << ":"
+                   << "\n-> white board will contain key '" << handle->key()
+                   << "'"
+                   << "\nat this point in the sequence, but the type will be\n"
+                   << "'" << boost::core::demangle(type.name()) << "'"
+                   << "\nand not\n"
+                   << "'" << boost::core::demangle(handle->typeInfo().name())
+                   << "'");
+        valid = false;
+      }
+    } else {
+      ACTS_ERROR("Adding " << elementType << " " << element->name() << ":"
+                           << "\n-> white board will not contain key"
+                           << "   '" << handle->key()
+                           << "' at this point in the sequence."
+                           << "\n   Needed for read data handle '"
+                           << handle->name() << "'")
+      valid = false;
+    }
+  }
+
+  if (valid) {  // only record outputs this if we're valid until here
+    for (const auto* handle : element->writeHandles()) {
+      if (!handle->isInitialized()) {
+        continue;
+      }
+
+      if (auto it = m_whiteBoardState.find(handle->key());
+          it != m_whiteBoardState.end()) {
+        ACTS_ERROR("White board will already contain key '"
+                   << handle->key() << "' (cannot overwrite)");
+        valid = false;
+        break;
+      }
+
+      m_whiteBoardState.emplace(std::pair{handle->key(), &handle->typeInfo()});
+
+      if (auto it = m_whiteboardObjectAliases.find(handle->key());
+          it != m_whiteboardObjectAliases.end()) {
+        ACTS_DEBUG("Key '" << handle->key() << "' aliased to '" << it->second
+                           << "'");
+        m_whiteBoardState[it->second] = &handle->typeInfo();
+      }
+    }
+  }
+
+  if (!valid) {
+    throw SequenceConfigurationException{};
+  }
+
+  ACTS_INFO("Added " << elementType << " '" << element->name() << "'");
+  auto symbol = [](const char* in) {
+    std::string s = boost::core::demangle(in);
+    if (s.size() > 80) {
+      s.erase(80);
+      s += "...";
+    }
+    return s;
+  };
+
+  for (const auto* handle : element->readHandles()) {
+    if (!handle->isInitialized()) {
+      continue;
+    }
+    ACTS_INFO("<- " << handle->name() << " '" << handle->key() << "':");
+    ACTS_INFO("   " << symbol(handle->typeInfo().name()));
+  }
+
+  for (const auto* handle : element->writeHandles()) {
+    if (!handle->isInitialized()) {
+      continue;
+    }
+    ACTS_INFO("-> " << handle->name() << " '" << handle->key() << "':");
+    ACTS_INFO("   " << symbol(handle->typeInfo().name()));
+  }
 }
 
 void Sequencer::addWhiteboardAlias(const std::string& aliasName,
